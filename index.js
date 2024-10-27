de837fc3bcdfd1adfa6cf516486d50fe80f1f0ca
Salvador Diaz <163175917+slvDiaz@users.noreply.github.com>
2024-10-26 18:52:41 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+function img1() {
+	var img=document.getElementById('bgDiv');
+	var border=document.getElementById('border1');
+	var border2=document.getElementById('border2');
+	var border3=document.getElementById('border3');
+	border.style.border=("1px solid #c1bb01");
+	border2.style.border=("1px solid #fff");
+	border3.style.border=("1px solid #fff");
+	var borderPrin=document.getElementById('borderPrin1');
+	var borderPrin2=document.getElementById('borderPrin2');
+	var borderPrin3=document.getElementById('borderPrin3');
+	borderPrin.style.borderBottom=("1px solid #c1bb01");
+	borderPrin2.style.borderBottom=("1px solid #fff");
+	borderPrin3.style.borderBottom=("1px solid #fff");
+	img.style.backgroundImage = "url('bg.jpeg')";
+}
+function img2() {
+	var img=document.getElementById('bgDiv');
+	var border=document.getElementById('border1');
+	var border2=document.getElementById('border2');
+	var border3=document.getElementById('border3');
+	border.style.border=("1px solid #fff");
+	border2.style.border=("1px solid #c1bb01");
+	border3.style.border=("1px solid #fff");
+	var borderPrin=document.getElementById('borderPrin1');
+	var borderPrin2=document.getElementById('borderPrin2');
+	var borderPrin3=document.getElementById('borderPrin3');
+	borderPrin.style.borderBottom=("1px solid #fff");
+	borderPrin2.style.borderBottom=("1px solid #c1bb01");
+	borderPrin3.style.borderBottom=("1px solid #fff");
+	img.style.backgroundImage = "url('bg2.jpg')";
+}
+function img3() {
+	var img=document.getElementById('bgDiv');
+	var border=document.getElementById('border1');
+	var border2=document.getElementById('border2');
+	var border3=document.getElementById('border3');
+	border.style.border=("1px solid #fff");
+	border2.style.border=("1px solid #fff");
+	border3.style.border=("1px solid #c1bb01");
+	var borderPrin=document.getElementById('borderPrin1');
+	var borderPrin2=document.getElementById('borderPrin2');
+	var borderPrin3=document.getElementById('borderPrin3');
+	borderPrin.style.borderBottom=("1px solid #fff");
+	borderPrin2.style.borderBottom=("1px solid #fff");
+	borderPrin3.style.borderBottom=("1px solid #c1bb01");
+	img.style.backgroundImage = "url('bg3.jpg')";
+}
